42e3d2eaeb4c093314d7ffc588663efc5879553f
Ghabryel <ghabryelhenrique@hotmail.com>
2022-04-30 00:49:51 -0300

    add login screen

---
+++ src/app/module/client/pages/client-home/client-home.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-home',
+  templateUrl: './client-home.page.html',
+  styleUrls: ['./client-home.page.scss'],
+})
+export class ClientHomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
